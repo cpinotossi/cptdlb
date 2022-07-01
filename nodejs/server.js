701ff7726a2c7e0f4a86d54561441e853040ed4a
cpinotossi <cpinotossi@web.de>
2022-07-01 14:38:16 +0200

    add color status to log files

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,23 +1,21 @@
 #!/usr/bin/env nodejs
 
-const https = require('https');
 const http = require('http');
-const fs = require('fs');
 const url = require('url');
 require('dotenv').config();
 const logger = require('./logger');
-
 
 const portHttp = process.env.PORTHTTP;
 const color = process.env.SCOLOR;
 const iip = process.env.INTERFACEIP;
 
 // Object which will be printed on server response
-let socketDetails = {
+let details = {
     ladd:'',
     lport:'',
     radd:'',
-    rport:''
+    rport:'',
+    color:''
 };
 
 // Create http server
@@ -25,23 +23,23 @@ var server = http.createServer();
 server.on('request',(req,res)=>{
     //Create some cpu load
     let f = (url.parse(req.url,true).query.f)?parseInt(url.parse(req.url,true).query.f):0;
-    socketDetails.ladd = req.socket.localAddress;
-    socketDetails.lport = req.socket.localPort;
-    socketDetails.radd = req.socket.remoteAddress;
-    socketDetails.rport = req.socket.remotePort;
-    //console.log(JSON.stringify(socketDetails, null, '\t'));
-    logger.info(socketDetails);
+    details.ladd = req.socket.localAddress;
+    details.lport = req.socket.localPort;
+    details.radd = req.socket.remoteAddress;
+    details.rport = req.socket.remotePort;
+    details.color = color;
+
+    logger.info(details);
     res.write(`<body bgcolor="${color}">\n`);
     res.write(`in:${f},out:${fibo(f)}\n`);
     res.write(`${JSON.stringify(req.headers, null, '\t')}\n`);
-    res.write(`${JSON.stringify(socketDetails, null, '\t')}\n`);   
+    res.write(`${JSON.stringify(details, null, '\t')}\n`);   
     res.write(`</body>\n`);
     res.end();
 });
 
 server.listen(portHttp,iip,()=>{
     logger.info(`ServerHTTP waiting on ${iip}:${portHttp} for you`);
-    //console.log(`ServerHTTP waiting on ${iip}:${portHttp} for you`)
 })
 
 function fibo(num){
